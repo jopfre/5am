11e18ca0e36c22dea0f36cae8c1014b9172e26a0
jopfre <jonah.freeland@gmail.com>
2018-12-19 00:45:45 +0530

    clean

--- routes.js
+++ routes.js
@@ -3,14 +3,6 @@ const routes = express.Router();
 
 const mongoClient = require('./mongo-client');
 
-const { PerformanceObserver, performance } = require('perf_hooks');
-
-const obs = new PerformanceObserver((items) => {
-  console.log(items.getEntries()[0].name+' took '+items.getEntries()[0].duration);
-  performance.clearMarks();
-});
-obs.observe({ entryTypes: ['measure'] });
-
 let db = mongoClient.getDb();
 const lidar = db.collection('lidar');
 
@@ -20,9 +12,6 @@ routes.route('/lidar')
     let lon = req.query.lon.substr(1);
     var id = 'st'+lat+lon;
 
-    console.log("Finding "+id);
-    performance.mark('A');
-
     lidar.findOne(
       {_id: id},
       {projection: { data: 1 }},
@@ -30,14 +19,12 @@ routes.route('/lidar')
       if(!doc) {
         console.log(id+' not found.');
       } else {
-        console.log(id+' found.')
         res.json(doc);
+        console.log(id+' found.')
       }
     }).catch(function(err){
       console.log(err);
     });
-    performance.mark('B');
-    performance.measure('Find '+id, 'A', 'B');
 
   });
 
