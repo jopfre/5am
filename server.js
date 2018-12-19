61aff68c976aed1312b420f056b55c666ecbdcbd
jopfre <jonah.freeland@gmail.com>
2018-12-19 08:56:34 +0530

    remove cors as all on same server now

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ mongoClient.connect((err) => {
 	app.use(bodyParser.urlencoded({ extended: true }));
 	
 	app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  // res.header("Access-Control-Allow-Origin", "*");
+	  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  next();
 	});
 
