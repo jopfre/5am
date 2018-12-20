adbe1a0b6d7468888473c836896aced6b37d77d8
jopfre <jonah.freeland@gmail.com>
2018-12-20 18:21:24 +0530

    sun times

--- public/src/js/sun.js
+++ public/src/js/sun.js
@@ -1,10 +1,11 @@
 var lat = bristolLatLon[0];
 var lon = bristolLatLon[1];
 var times = SunCalc.getTimes(new Date(), lat, lon);
-var sunriseStr = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
+var sunriseTime = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
+var sunsetTime = times.sunset.getHours() + ':' + times.sunset.getMinutes();
 var sunrisePos = SunCalc.getPosition(times.sunrise, lat, lon);
 console.log(times);
-console.log(sunriseStr);
 console.log(sunrisePos);
 
-document.getElementById('sunrise-time').textContent = sunriseStr;
+document.getElementById('sunrise-time').textContent = sunriseTime;
+document.getElementById('sunset-time').textContent = sunsetTime;
